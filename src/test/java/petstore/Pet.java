63995a2be1e1c67ca24eb1bec5bbaf990eab61ed
natecp <n4t34546544>
2021-09-07 09:16:23 -0300

    Conclusão do script consultarPet (Get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 //3 - Classe
 public class Pet {
@@ -18,18 +19,18 @@ public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
     private Object IOException;
 
-    // 3.2 - MÃ©todos e FunÃ§Ãµes
+    // 3.2 - Métodos e Funções
     public String ler_json(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Post
-    @Test
+    @Test(priority = 1)
     public void incluir_pet() throws IOException {
         String jsonBody = ler_json("db/pet1.json");
 
         //Sintaxe Gherkin - Estrutura Rest-Assured
-        //Dado - Quando - EntÃ£o
+        //Dado - Quando - Então
         //Given - When - Then
 
         String path;
@@ -45,10 +46,43 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is ("klaus"))
                 .body( "status", is ("available"))
+                .body ("category.name", is ("132NC456DP")) //usar IS quando não tem colchetes dentro do principal
+                .body ("tags.name", contains("token")) //usar CONTAINS quando tem colchetes dentro do principal
         ;
 
 
 
     }
+
+    //GET
+@Test(priority = 2)
+    public void consultarPet(){
+        String petId = "1991030830";
+        String token =
+            given()
+                    .contentType("application/json")
+                    .log().all()
+
+                    .when()
+                            .get(uri + "/" + petId)
+
+                    .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("name", is ("klaus"))
+                    .body("status", is ("available"))
+                    .body("category.name", is ("132NC456DP"))
+
+
+                    .extract()
+                .path("category.name")
+
+           ;
+
+    System.out.println("O token é " + token);
+
+
+    }
+
     }
 
